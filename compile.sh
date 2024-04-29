457c74faf31fde967430a800f3678a06800706ae
Tyler <tylergross28@gmail.com>
2024-04-29 14:33:09 -0400

    compile flags

---
+++ compile.sh
@@ -0,0 +1,12 @@
+emcc \
+-sENVIRONMENT=web,worker \
+-sALLOW_MEMORY_GROWTH=1 \
+-sMAXIMUM_MEMORY=4GB \
+-pthread \
+-sPTHREAD_POOL_SIZE=navigator.hardwareConcurrency \
+-lembind \
+-Oz \
+-std=c++17 \
+--embind-emit-tsd lib.d.ts \
+./src/lib.cpp \
+-o ./dist/lib.js
